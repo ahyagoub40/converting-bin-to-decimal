77ff241e7eb8bbcc121b7955f672d2617ff5810c
Ahmed Yagoub <ahyagoub40@gmail.com>
2020-07-30 11:13:20 -0600

    modified css

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 const InputBox = props => {
   return (
     <form className="text-center">
--- src/Components/Result.js
+++ src/Components/Result.js
@@ -1,5 +1,4 @@
 import React from 'react'
-
 const ResultsBox = props => {
   return (
     <div className="text-center result">
